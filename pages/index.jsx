d6ded37cffa33f6c398bbee1ab2440331236a0a2
Sam Magura <srmagura@gmail.com>
2022-07-03 15:03:22 -0400

    Simplify the minimal repro

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
-import Errors from "../components/Errors";
-import NoError from "../components/NoError";
+import styled from "@emotion/styled";
+
+const Icon = styled.span`
+  &::before {
+    content: "\u203A";
+    color: green;
+  }
+`;
 
 export default function IndexPage() {
-  return (
-    <div>
-      <Errors />
-      <NoError />
-    </div>
-  );
+  return <Icon />;
 }
